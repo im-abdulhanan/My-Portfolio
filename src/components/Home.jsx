7da35c851e3532d2bd56675418d7fd100b4fab32
im-abdulhanan <abdulhanan.microsoft@gmail.com>
2026-03-12 22:33:05 +0500

    files added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
+import "../css components/Home.css"
 export const Home = () => {
   return (
     <div className="w-full h-[88vh] flex justify-center items-center bg-red-400">
         <div className="w-[50%] h-[80vh] bg-black flex flex-col items-center justify-around">
             <div className="flex  gap-2 items-center w-fit h-fit ">
-                <div className="w-[15px] h-[15px] rounded-[100%] bg-green-300 flex items-center justify-center">
-                    <div className="w-[7px] h-[7px] rounded-[100%] bg-green-500"></div></div>
+                <div className="w-[10px] h-[10px] rounded-[100%] bg-green-500 green-small"></div>
                 <h4 className="text-white/70">Available For Work</h4>
             </div>
             <div className="">
