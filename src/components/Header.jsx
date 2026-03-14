1da7bcb662a35782c942ee5090e9b4c1fb9f9f8c
im-abdulhanan <abdulhanan.microsoft@gmail.com>
2026-03-15 00:44:23 +0500

    files added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react"
+import { FiAlignJustify } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
 export const Header = () => {
   const menuNames = ["HOME","ABOUT","PROJECTS","CONTACT"]
   const [bgColor ,setBgColor] = useState("HOME")
+  const [open, setOpen] = useState(false)
   return (
     <nav className="bg-black">
       <div className="hidden sm:flex w-full h-35">
@@ -17,8 +20,20 @@ export const Header = () => {
         </ul>
         </div>
       </div>
-      <div className="flex flex-col  sm:hidden">
-       <h1 className="text-white">hi</h1>
+      <div className="flex justify-between px-5 items-center h-20 bg-black sm:hidden">
+        <h1 className="text-white text-3xl">AH</h1>
+        <h3 onClick={()=>setOpen(!open)} className="text-3xl">{open ? <IoMdClose className="text-white" />  : <FiAlignJustify className="text-white"/>}</h3 >
+      </div>
+      <div className={`${open ? "flex" : "hidden"} flex-col items-center justify-around sm:hidden h-screen`}>
+        <ul className="flex flex-col justify-around items-center w-[50%]  h-[60%] ">
+            {menuNames.map((item)=>(
+            <li key={item} className={`sm:px-4 sm:py-2 px-6 py-2 rounded-4xl
+              ${bgColor === item ? "bg-white text-black" : "hover:bg-white/5 text-white"}
+              `}
+              onClick={()=>setBgColor(item)}            
+            >{item}</li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
